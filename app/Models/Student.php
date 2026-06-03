ad29c7fef6c0463d9793d9ab1d3f0c47d4c137a3
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-06-03 12:52:09 +0700

    refactor: turn harddelete to softdelete

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,16 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'nis',
         'class',
         'parent_phone'
     ];
-    
+
     public function bills()
     {
         return $this->hasMany(Bill::class);

a020692785ea9bca90379681299b13213c9db4e3
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-06-03 19:03:49 +0700

    refactor: add a new structure sidebar folder

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -25,30 +25,30 @@
         <nav class="mt-4 space-y-2">
 
             @if (auth()->user()->isSuperAdmin())
-                <x-sidebar-link :href="route('dashboard')" icon="ri-dashboard-line" :active="request()->routeIs('dashboard')">
+                <x-sidebar.sidebar-link :href="route('dashboard')" icon="ri-dashboard-line" :active="request()->routeIs('dashboard')">
                     Dashboard
-                </x-sidebar-link>
+                </x-sidebar.sidebar-link>
             @endif
 
-            <x-sidebar-link :href="route('students.index')" icon="ri-graduation-cap-line" :active="request()->routeIs('students.*')">
+            <x-sidebar.sidebar-link :href="route('students.index')" icon="ri-graduation-cap-line" :active="request()->routeIs('students.*')">
                 Student Records
-            </x-sidebar-link>
+            </x-sidebar.sidebar-link>
 
-            <x-sidebar-link :href="route('bills.index')" icon="ri-file-list-3-line" :active="request()->routeIs('bills.index')">
+            <x-sidebar.sidebar-link :href="route('bills.index')" icon="ri-file-list-3-line" :active="request()->routeIs('bills.index')">
                 List Bills
-            </x-sidebar-link>
+            </x-sidebar.sidebar-link>
 
-            <x-sidebar-link :href="route('bills.generate.form')" icon="ri-file-settings-line" :active="request()->routeIs('bills.generate.form')">
+            <x-sidebar.sidebar-link :href="route('bills.generate.form')" icon="ri-file-settings-line" :active="request()->routeIs('bills.generate.form')">
                 Generate Bills
-            </x-sidebar-link>
+            </x-sidebar.sidebar-link>
 
-            <x-sidebar-dropdown title="Reports" icon="ri-book-2-line" :active="request()->routeIs('reports.*')">
+            <x-sidebar.sidebar-dropdown title="Reports" icon="ri-book-2-line" :active="request()->routeIs('reports.*')">
 
-                <x-sidebar-dropdown-link :href="route('reports.overdue-report')" :active="request()->routeIs('reports.overdue-report')">
+                <x-sidebar.sidebar-dropdown-link :href="route('reports.overdue-report')" :active="request()->routeIs('reports.overdue-report')">
                 Overdue Report
-                </x-sidebar-dropdown-link>
+                </x-sidebar.sidebar-dropdown-link>
 
-            </x-sidebar-dropdown>
+            </x-sidebar.sidebar-dropdown>
         </nav>
 
     </aside>

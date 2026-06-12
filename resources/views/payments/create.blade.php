d9ceff75de8d01071f6cb3766658266362806451
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-06-12 14:47:18 +0700

    style: adjust create payment layout for mobile and tablet view

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -5,94 +5,90 @@
             <h1 class="font-semibold text-xl">SPP Payment</h1>
         </div>
 
-        <div class="">
-            <div class="bg-white overflow-hidden shadow-sm border-4 border-double border-gray-400">
-                <div class="p-6">
-                    <div class="flex text-md">
-                        <div class="w-1/2">
-                            <p>
-                                Nama Siswa:
-                                <br>
-                                <span class="font-semibold">
-                                    {{ $bill->student->name }}
-                                </span>
-                            </p>
+        <div class="bg-white overflow-hidden shadow-sm border-4 border-double border-gray-400">
+            <div class="p-6">
+                <div class="flex text-md">
+                    <div class="w-1/2">
+                        <p>
+                            Nama Siswa:
                             <br>
-                            <p>
-                                Periode:
-                                <br>
-                                <span class="font-semibold">
-                                    {{ $bill->billing_period }}
-                                </span>
-                            </p>
+                            <span class="font-semibold">
+                                {{ $bill->student->name }}
+                            </span>
+                        </p>
+                        <br>
+                        <p>
+                            Periode:
+                            <br>
+                            <span class="font-semibold">
+                                {{ $bill->billing_period }}
+                            </span>
+                        </p>
+                    </div>
+                    <div class="w-1/2">
+                        <p>
+                            Nominal:
+                            <br>
+                            <span class="font-semibold">
+                                {{ rupiah($bill->amount) }}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+
+                <hr class="my-5 border-1 border-dashed border-gray-400">
+
+                <form action="{{ route('payments.store', $bill) }}" method="POST">
+                    @csrf
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-3">
+                        <div class="input-group">
+                            <x-form.input-label for="name" :value="__('Payment Method')" />
+                            <div class="flex gap-4 mt-1">
+                                <x-form.radio id="cash" name="payment_method_id" label="Cash" value="1"
+                                    :checked="old('payment_method_id') == 1" />
+
+                                <x-form.radio id="transfer" name="payment_method_id" label="Transfer" value="2"
+                                    :checked="old('payment_method_id') == 2" />
+                            </div>
+                            <x-form.input-error :messages="$errors->get('payment_method_id')" />
                         </div>
-                        <div class="w-1/2">
-                            <p>
-                                Nominal:
-                                <br>
-                                <span class="font-semibold">
-                                    {{ rupiah($bill->amount) }}
-                                </span>
-                            </p>
+
+                        <div class="input-group">
+                            <x-form.input-label for="paid_at" :value="__('Payment Date')" />
+                            <x-form.text-input id="paid_at" class="mt-1 w-full" type="datetime-local" name="paid_at"
+                                :value="old('paid_at')" required autofocus autocomplete="paid_at" />
+                            <x-form.input-error :messages="$errors->get('paid_at')" />
                         </div>
                     </div>
 
-                    <hr class="my-5 border-1 border-dashed border-gray-400">
-
-                    <form action="{{ route('payments.store', $bill) }}" method="POST">
-                        @csrf
-
-                        <div class="flex gap-4">
-                            <div class="input-group w-1/2">
-                                <x-form.input-label for="name" :value="__('Payment Method')" />
-                                <div class="flex gap-4 mt-1">
-
-                                    <x-form.radio id="cash" name="payment_method_id" label="Cash" value="1"
-                                        :checked="old('payment_method_id') == 1" />
-
-                                    <x-form.radio id="transfer" name="payment_method_id" label="Transfer"
-                                        value="2" :checked="old('payment_method_id') == 2" />
-
-                                </div>
-                                <x-form.input-error :messages="$errors->get('payment_method_id')" />
-                            </div>
-
-                            <div class="input-group w-1/2">
-                                <x-form.input-label for="paid_at" :value="__('Payment Date')" />
-                                <x-form.text-input id="paid_at" class="block mt-1 w-full" type="datetime-local"
-                                    name="paid_at" :value="old('paid_at')" required autofocus autocomplete="paid_at" />
-                                <x-form.input-error :messages="$errors->get('paid_at')" />
-                            </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                        <div class="input-group">
+                            <x-form.input-label for="amount_paid" :value="__('Total Amount')" />
+                            <x-form.text-input id="amount_paid" class="block mt-1 w-full" type="number"
+                                name="amount_paid" :value="$bill->amount" required autofocus autocomplete="amount_paid"
+                                readonly />
+                            <x-form.input-error :messages="$errors->get('amount_paid')" />
                         </div>
 
-                        <div class="flex gap-4 mt-3">
-                            <div class="input-group w-1/2">
-                                <x-form.input-label for="amount_paid" :value="__('Total Amount')" />
-                                <x-form.text-input id="amount_paid" class="block mt-1 w-full" type="number"
-                                    name="amount_paid" :value="$bill->amount" required autofocus autocomplete="amount_paid"
-                                    readonly />
-                                <x-form.input-error :messages="$errors->get('amount_paid')" />
-                            </div>
-
-                            <div class="input-group w-1/2">
-                                <x-form.input-label for="notes" :value="__('Notes')" />
-                                <x-form.textarea name="notes" rows="4" class="w-full">
-                                    {{ old('notes') }}
-                                </x-form.textarea>
-                                <x-form.input-error :messages="$errors->get('notes')" />
-                            </div>
+                        <div class="input-group">
+                            <x-form.input-label for="notes" :value="__('Notes')" />
+                            <x-form.textarea name="notes" rows="4" class="w-full">
+                                {{ old('notes') }}
+                            </x-form.textarea>
+                            <x-form.input-error :messages="$errors->get('notes')" />
                         </div>
+                    </div>
 
-                        <div class="flex justify-end mt-6 gap-2">
-                            <x-link-button.secondary-link :href="url()->previous()">
-                                Back
-                            </x-link-button.secondary-link>
-                            <x-button.primary-button>
-                                {{ __('Save Payment') }}
-                            </x-button.primary-button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="flex justify-end mt-6 gap-2">
+                        <x-link-button.secondary-link :href="url()->previous()">
+                            Back
+                        </x-link-button.secondary-link>
+                        <x-button.primary-button>
+                            {{ __('Save Payment') }}
+                        </x-button.primary-button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>

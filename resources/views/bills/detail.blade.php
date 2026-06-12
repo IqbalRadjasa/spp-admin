86fd250f82b09b79fbc636ecfcdfaf6659568283
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-06-12 14:33:22 +0700

    style: adjust detail bill layout for mobile and tablet view

--- resources/views/bills/detail.blade.php
+++ resources/views/bills/detail.blade.php
@@ -11,113 +11,115 @@
 
         <div class="">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="sect-1 mb-10">
-                        <h2 class="font-bold text-lg">Bill's Information</h2>
+                <div class="p-6">
+
+                    {{-- Student Detail --}}
+                    <div class="mb-10">
+                        <h2 class="font-bold text-lg">Student Information</h2>
                         <hr class="mb-5">
 
-                        <div class="flex">
-                            {{-- Bill's Detail --}}
-                            <div class="w-1/2">
-                                <table class="text-sm">
-                                    <tbody>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Billing Period</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ $bill->billing_period }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Amount</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ rupiah($bill->amount) }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Status</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">
-                                                <span
-                                                    class="{{ $bill->status == 'paid' ? 'bg-green-100 text-green-500' : 'bg-red-100 text-red-500' }} py-1 px-2 text-sm font-semibold rounded-md">
-                                                    {{ titleCase($bill->status) }}
+                        <table class="text-sm">
+                            <tbody>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Student's Name</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ $bill->student->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">NIS</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ $bill->student->nis }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Class</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ $bill->student->class }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Parent's Phone</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ $bill->student->parent_phone }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {{-- Bill Detail --}}
+                    <div class="mb-10">
+                        <h2 class="font-bold text-lg">Bill Information</h2>
+                        <hr class="mb-5">
+
+                        <table class="text-sm">
+                            <tbody>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Billing Period</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ $bill->billing_period }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Amount</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">{{ rupiah($bill->amount) }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-3 whitespace-nowrap">Status</td>
+                                    <td class="pr-5 pb-3">:</td>
+                                    <td class="pb-3">
+                                        <span
+                                            class="{{ $bill->status == 'paid' ? 'bg-green-100 text-green-500' : 'bg-red-100 text-red-500' }} py-1 px-2 text-sm font-semibold rounded-md">
+                                            {{ titleCase($bill->status) }}
+                                        </span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="font-semibold pr-4 md:pr-10 pb-5 whitespace-nowrap">Escalation Status
+                                    </td>
+                                    <td class="pr-5 pb-5">:</td>
+                                    <td class="pb-5">
+                                        @switch($bill->escalation_status)
+                                            @case('resolved')
+                                                <span class="bg-green-100 text-green-500 font-semibold py-1 px-2  ">
+                                                    {{ titleCase($bill->escalation_status) }}
                                                 </span>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-5">Escalation Status</td>
-                                            <td class="pr-5 pb-5">:</td>
-                                            <td class="pb-5">
-                                                @switch($bill->escalation_status)
-                                                    @case('resolved')
-                                                        <span class="bg-green-100 text-green-500 font-semibold py-1 px-2  ">
-                                                            {{ titleCase($bill->escalation_status) }}
-                                                        </span>
-                                                    @break
+                                            @break
 
-                                                    @case('escalated')
-                                                        <span
-                                                            class="bg-red-100 text-red-500 font-semibold py-1 px-2 rounded-md">
-                                                            {{ titleCase($bill->escalation_status) }}
-                                                        </span>
-                                                    @break
+                                            @case('escalated')
+                                                <span class="bg-red-100 text-red-500 font-semibold py-1 px-2 rounded-md">
+                                                    {{ titleCase($bill->escalation_status) }}
+                                                </span>
+                                            @break
 
-                                                    @default
-                                                        <span
-                                                            class="bg-gray-100 text-gray-700 font-semibold py-1 px-2 rounded-md">
-                                                            {{ titleCase($bill->escalation_status) }}
-                                                        </span>
-                                                @endswitch
-                                            </td>
-                                        </tr>
-                                        @if ($bill->escalation_status === 'escalated')
-                                            <tr>
-                                                <td class="font-semibold pr-10 pb-2">Escalation Notes</td>
-                                                <td class="pr-5">:</td>
-                                                <td>
-                                                    <form action="{{ route('bills.escalation-notes.store', $bill) }}"
-                                                        class="flex gap-2" method="POST">
-                                                        @csrf
+                                            @default
+                                                <span class="bg-gray-100 text-gray-700 font-semibold py-1 px-2 rounded-md">
+                                                    {{ titleCase($bill->escalation_status) }}
+                                                </span>
+                                        @endswitch
+                                    </td>
+                                </tr>
+                                @if ($bill->escalation_status === 'escalated')
+                                    <tr>
+                                        <td colspan="3" class="font-semibold pr-4 md:pr-10 pb-2 whitespace-nowrap">
+                                            Escalation Notes
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">
+                                            <form action="{{ route('bills.escalation-notes.store', $bill) }}"
+                                                class="flex gap-2" method="POST">
+                                                @csrf
 
-                                                        <x-form.text-input class="w-full" type="text" name="note"
-                                                            :value="old('note')" />
+                                                <x-form.text-input class="w-full" type="text" name="note"
+                                                    :value="old('note')" />
 
-                                                        <x-button.primary-button>
-                                                            {{ __('Save') }}
-                                                        </x-button.primary-button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endif
-                                    </tbody>
-                                </table>
-                            </div>
-
-                            {{-- Student's Detail --}}
-                            <div class="w-1/2">
-                                <table class="text-sm">
-                                    <tbody>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Student's Name</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ $bill->student->name }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">NIS</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ $bill->student->nis }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Class</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ $bill->student->class }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="font-semibold pr-10 pb-3">Parent's Phone</td>
-                                            <td class="pr-5 pb-3">:</td>
-                                            <td class="pb-3">{{ $bill->student->parent_phone }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                                                <x-button.primary-button>
+                                                    {{ __('Save') }}
+                                                </x-button.primary-button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
                     </div>
 
 

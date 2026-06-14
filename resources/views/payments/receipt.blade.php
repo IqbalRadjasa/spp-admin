1867d93d3646c8574e57a7619d1a8a372c9e57f9
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-06-14 07:03:50 +0700

    style: adjust print receipt layout for mobile and tablet view

--- resources/views/payments/receipt.blade.php
+++ resources/views/payments/receipt.blade.php
@@ -50,10 +50,10 @@
 
 <x-app-layout>
     <div class="py-6">
-        <div class="flex items-center pb-6 justify-between no-print">
+        <div class="flex flex-col sm:flex-row md:items-center md:justify-between pb-6 gap-2 no-print">
             <h1 class="font-semibold text-xl">Receipt Payment</h1>
 
-            <div class="gap-2">
+            <div class="flex flex-col sm:flex-row gap-2">
                 <x-link-button.secondary-link :href="route('reports.payment-report')">
                     Back
                 </x-link-button.secondary-link>
@@ -64,93 +64,95 @@
             </div>
         </div>
 
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="receipt p-6">
+        <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+            <div class="overflow-x-auto">
+                <div class="receipt p-6 min-w-[850px] max-w-[850px] mx-auto">
 
-                {{-- Header --}}
-                <div class="bg-gray-700 text-white text-2xl font-bold text-center py-7">
-                    <h1 class="">
-                        School Tuition Receipt
-                    </h1>
+                    {{-- Header --}}
+                    <div class="bg-gray-700 text-white text-2xl font-bold text-center py-7">
+                        <h1 class="">
+                            School Tuition Receipt
+                        </h1>
+                    </div>
+
+                    {{-- School --}}
+                    <div class="bg-gray-200 text-center py-2 mb-12">
+                        <h1 class="text-lg">
+                            Your School Name
+                        </h1>
+
+                        {{-- Address --}}
+                        <span class="text-sm">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ducimus quibusdam tempore
+                        </span>
+                    </div>
+
+                    {{-- Receipt Code --}}
+                    <div class="flex flex-col justify-end text-end text-sm mb-8">
+                        <span class="font-semibold">
+                            Receipt Code
+                        </span>
+
+                        <span class="bg-gray-700 text-white py-2 px-2 self-end">
+                            {{ $payment->payment_code }}
+                        </span>
+                    </div>
+
+                    {{-- Information --}}
+                    <div class="text-base/7 font-medium mb-10">
+                        <p>
+                            Name of Student:
+                            {{ $payment->bill->student->name }}
+                        </p>
+
+                        <p>
+                            NIS:
+                            {{ $payment->bill->student->nis }}
+                        </p>
+
+                        <p>
+                            Period:
+                            {{ $payment->bill->billing_period }}
+                        </p>
+
+                        <p>
+                            Payment Date:
+                            {{ $payment->paid_at }}
+                        </p>
+
+                        <p>
+                            Payment Method:
+                            {{ $payment->paymentMethod->name }}
+                        </p>
+
+                        <p>
+                            Note:
+                            {{ $payment->notes ?? '-' }}
+                        </p>
+                    </div>
+
+                    {{-- Table Payment Information --}}
+                    <table class="text-center">
+                        <thead>
+                            <tr>
+                                <th class="border border-gray-500 px-3 py-2">No</th>
+                                <th class="border border-gray-500 px-20 py-2">Particulars</th>
+                                <th class="border border-gray-500 px-16 py-2">Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border border-gray-500 px-3 py-2">1</td>
+                                <td class="border border-gray-500 px-20 py-2">Tuition Fee</td>
+                                <td class="border border-gray-500 px-16 py-2">{{ rupiah($payment->bill->amount) }}</td>
+                            </tr>
+                            <tr class="bg-gray-300 font-semibold">
+                                <td class="border border-gray-500 px-20 py-2" colspan="2">Total</td>
+                                <td class="border border-gray-500 px-16 py-2">{{ rupiah($payment->bill->amount) }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-
-                {{-- School --}}
-                <div class="bg-gray-200 text-center py-2 mb-12">
-                    <h1 class="text-lg">
-                        Your School Name
-                    </h1>
-
-                    {{-- Address --}}
-                    <span class="text-sm">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ducimus quibusdam tempore
-                    </span>
-                </div>
-
-                {{-- Receipt Code --}}
-                <div class="flex flex-col justify-end text-end text-sm mb-8">
-                    <span class="font-semibold">
-                        Receipt Code
-                    </span>
-
-                    <span class="bg-gray-700 text-white py-2 px-2 self-end">
-                        {{ $payment->payment_code }}
-                    </span>
-                </div>
-
-                {{-- Information --}}
-                <div class="text-base/7 font-medium mb-10">
-                    <p>
-                        Name of Student:
-                        {{ $payment->bill->student->name }}
-                    </p>
-
-                    <p>
-                        NIS:
-                        {{ $payment->bill->student->nis }}
-                    </p>
-
-                    <p>
-                        Period:
-                        {{ $payment->bill->billing_period }}
-                    </p>
-
-                    <p>
-                        Payment Date:
-                        {{ $payment->paid_at }}
-                    </p>
-
-                    <p>
-                        Payment Method:
-                        {{ $payment->paymentMethod->name }}
-                    </p>
-
-                    <p>
-                        Note:
-                        {{ $payment->notes ?? '-' }}
-                    </p>
-                </div>
-
-                {{-- Table Payment Information --}}
-                <table class="text-center">
-                    <thead>
-                        <tr>
-                            <th class="border border-gray-500 px-3 py-2">No</th>
-                            <th class="border border-gray-500 px-20 py-2">Particulars</th>
-                            <th class="border border-gray-500 px-16 py-2">Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="border border-gray-500 px-3 py-2">1</td>
-                            <td class="border border-gray-500 px-20 py-2">Tuition Fee</td>
-                            <td class="border border-gray-500 px-16 py-2">{{ rupiah($payment->bill->amount) }}</td>
-                        </tr>
-                        <tr class="bg-gray-300 font-semibold">
-                            <td class="border border-gray-500 px-20 py-2" colspan="2">Total</td>
-                            <td class="border border-gray-500 px-16 py-2">{{ rupiah($payment->bill->amount) }}</td>
-                        </tr>
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
